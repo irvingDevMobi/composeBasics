b34a7a3e233f15f2e13f546b452c3e79f54fb488
Irving <irving.dev.mobi@gmail.com>
2022-03-22 19:00:19 -0600

    [step 4] Creating columns and rows

--- app/src/main/java/dev/irving/basiccodelab/MainActivity.kt
+++ app/src/main/java/dev/irving/basiccodelab/MainActivity.kt
@@ -3,14 +3,10 @@ package dev.irving.basiccodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,13 +49,23 @@ fun Greeting(name: String) {
         modifier = Modifier
             .padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(24.dp)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(24.dp)
         ) {
-            Text(text = "Hello,")
-            Text(text = name)
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+
+            OutlinedButton(
+                onClick = { /*TODO*/ },
+            ) {
+                Text(text = "Show more")
+            }
         }
     }
 }
