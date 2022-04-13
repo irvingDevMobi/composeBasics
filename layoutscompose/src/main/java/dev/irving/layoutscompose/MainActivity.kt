1744b24b3bcda3cc9672c33a694a8d427aa5d5c5
Irving <irving.dev.mobi@gmail.com>
2022-04-13 18:33:50 -0500

    [Code Lab Layouts] 6 Working with lists

--- layoutscompose/src/main/java/dev/irving/layoutscompose/MainActivity.kt
+++ layoutscompose/src/main/java/dev/irving/layoutscompose/MainActivity.kt
@@ -1,16 +1,14 @@
-@file:OptIn(ExperimentalMaterialApi::class)
-
 package dev.irving.layoutscompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -18,20 +16,24 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import dev.irving.layoutscompose.ui.theme.LayoutComposeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutComposeTheme {
-                LayoutCodeLab()
+                //LayoutCodeLab()
+                LazyList()
             }
         }
     }
@@ -53,9 +55,11 @@ fun LayoutCodeLab() {
             )
         }
     ) { innerPadding ->
-        BodyContent(modifier = Modifier
-            .padding(innerPadding)
-            .padding(8.dp))
+        BodyContent(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(8.dp)
+        )
     }
 }
 
@@ -66,6 +70,58 @@ fun BodyContent(modifier: Modifier) {
     ) {
         Text(text = "Hello Devs!!")
         Text(text = "Thanks for be a good developer!!")
+    }
+}
+
+@Composable
+fun LazyList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LazyListPreview() {
+    LayoutComposeTheme {
+        LazyList()
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
